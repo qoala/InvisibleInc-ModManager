919ab10d1984c7369cba152b807c93c4150ad803
Qoalabear <ed.gonzalez3@gmail.com>
2020-06-21 01:57:28 -0700

    Action to list installed mods

--- actions.py
+++ actions.py
@@ -21,6 +21,18 @@ class ListDownloadedAction:
         modspec_converter.write_modspec(mods, sys.stdout)
 
 
+class ListInstalledAction:
+    """Lists all installed mods."""
+    _config: Config
+
+    def __init__(self, config: Config) -> None:
+        self._config = config
+
+    def run(self):
+        mods = modinfo_parser.all_from_dir(self._config.install_path)
+        modspec_converter.write_modspec(mods, sys.stdout)
+
+
 class InstallAllAction:
     """Install all downloaded mods."""
     _config: Config
@@ -60,5 +72,6 @@ if __name__ == '__main__':
 
     config = Config(install_path=sys.argv[1], download_path=sys.argv[2])
     # ListDownloadedAction(config).run()
+    # ListInstalledAction(config).run()
     # InstallAllAction(config).run()
     # InstallFromSpecAction(config).run(sys.stdin)
