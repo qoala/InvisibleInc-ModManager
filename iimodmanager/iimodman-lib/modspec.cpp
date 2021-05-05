7dcbfd6c51629b1b43a1f93e371eb0bfa3a6c101
Qoalabear <512290+qoala@users.noreply.github.com>
2021-05-04 23:08:51 -0700

    Implementation for reading a modspec file

--- iimodmanager/iimodman-lib/modspec.cpp
+++ iimodmanager/iimodman-lib/modspec.cpp
@@ -1,7 +1,9 @@
 #include "modspec.h"
 
+#include <QDebug>
 #include <QList>
 #include <QString>
+#include <stdexcept>
 
 namespace iimodmanager {
 
@@ -9,6 +11,12 @@ namespace iimodmanager {
 // free text: human readable text with the mod name (and version specifier if versioned and available)
 // unused: In the future, could add a single-line json string or other format for additional fields.
 const QString specLineFormat = QStringLiteral("%1::%2::%3");
+const qsizetype specLineModId = 0;
+// unused: 1
+const qsizetype specLineVersionId = 2;
+// unused: 3
+const qsizetype specLineNameStart = 4;
+const qsizetype specLineSectionLength = 5;
 
 class ModSpec::Impl
 {
@@ -16,6 +24,8 @@ public:
     Impl(const QList<SpecMod> &mods);
 
     QList<SpecMod> mods;
+
+    bool readFromFile(QIODevice &file);
 };
 
 class SpecMod::Impl
@@ -46,9 +56,36 @@ const QList<SpecMod> ModSpec::mods() const
     return impl->mods;
 }
 
+bool ModSpec::readFromFile(QIODevice &file)
+{
+    return impl->readFromFile(file);
+}
+
 ModSpec::Impl::Impl(const QList<SpecMod> &mods)
     : mods(mods)
 {}
+
+bool ModSpec::Impl::readFromFile(QIODevice &file)
+{
+    if (file.isOpen() || file.open(QIODevice::ReadOnly|QIODevice::Text))
+    {
+        mods.clear();
+        QTextStream in(&file);
+        while (!in.atEnd())
+        {
+            QString line = in.readLine().simplified();
+            // skip blank lines and comment lines
+            if (line.isEmpty() || line.startsWith('#'))
+                continue;
+
+            std::optional<SpecMod> mod = SpecMod::fromSpecString(line);
+            if (mod)
+                mods.append(*mod);
+        }
+        return true;
+    }
+    return false;
+}
 
 SpecMod::SpecMod(const QString &id, const QString &name)
     : SpecMod(id, QString(), name, QString())
@@ -57,6 +94,24 @@ SpecMod::SpecMod(const QString &id, const QString &name)
 SpecMod::SpecMod(const QString &id, const QString &versionId, const QString &name, const QString &versionName)
     : impl{std::make_shared<Impl>(id, versionId, name, versionName)}
 {}
+
+std::optional<SpecMod> SpecMod::fromSpecString(const QString &line, const QString &debugRef, qsizetype debugLineNo)
+{
+    const QStringList list = line.split(':');
+    if (list.length() < specLineSectionLength)
+    {
+        const QString ref = debugLineNo >= 0 ? QStringLiteral(" (%1:%2)").arg(debugRef, debugLineNo): QString();
+        qWarning() << "Couldn't parse spec line" << ref << ": Too few separators. \"" << line << '"';
+        return {};
+    }
+
+    const QString &modId = list[specLineModId];
+    const QString &versionId = list[specLineVersionId];
+
+    const QString name = line.section(':', specLineNameStart);
+
+    return SpecMod(modId, versionId, name, QString());
+}
 
 QString SpecMod::id() const
 {
--- iimodmanager/iimodman-lib/modspec.h
+++ iimodmanager/iimodman-lib/modspec.h
@@ -8,6 +8,7 @@
 #include <QString>
 #include <memory>
 
+class QIODevice;
 template <typename T> class QList;
 
 namespace iimodmanager {
@@ -17,25 +18,27 @@ class SpecMod;
 //! List of mods in a mod-specification. A current or prospective state for installed mods.
 class IIMODMANLIBSHARED_EXPORT ModSpec
 {
+
 public:
     ModSpec(const QList<SpecMod> &mods);
 
     QList<SpecMod> mods();
     const QList<SpecMod> mods() const;
 
+    bool readFromFile(QIODevice &file);
+
 private:
     class Impl;
     std::experimental::propagate_const<std::unique_ptr<Impl>> impl;
 };
 
-class SpecMod
+class IIMODMANLIBSHARED_EXPORT SpecMod
 {
 public:
     SpecMod(const QString &id, const QString &name);
     SpecMod(const QString &id, const QString &versionId, const QString &name, const QString &versionName);
 
-    static SpecMod forModId(const QString &id);
-    static SpecMod forModVersionId(const QString &id, const QString &versionId);
+    static std::optional<SpecMod> fromSpecString(const QString &line, const QString &debugRef = QString(), qsizetype debugLine = -1);
 
     QString id() const;
     QString versionId() const;
@@ -45,20 +48,11 @@ public:
     QString asSpecString() const;
     QString asVersionedSpecString() const;
 
+
 private:
     class Impl;
     std::shared_ptr<const Impl> impl;
 };
-
-inline SpecMod SpecMod::forModId(const QString &id)
-{
-    return SpecMod(id, QString());
-}
-
-inline SpecMod SpecMod::forModVersionId(const QString &id, const QString &versionId)
-{
-    return SpecMod(id, versionId, QString(), QString());
-}
 
 } // namespace iimodmanager
 
